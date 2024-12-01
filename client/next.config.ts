dbacfbbbaf69be537b1b397773fe51933e864dae
Nguyen Duc Binh <tripplen63@gmail.com>
2024-12-01 17:23:14 +0200

    - Setup Turbo repo - Docker compose for server container

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
 };
 
 export default nextConfig;
